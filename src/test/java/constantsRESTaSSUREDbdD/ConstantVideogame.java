376bd76dfdf47d1e08a21b1a331f018e88daca50
ggosw <ggosw@192.168.86.30>
2020-03-11 21:49:26 -0700

    First Repository

--- src/test/java/constantsRESTaSSUREDbdD/ConstantVideogame.java
+++ src/test/java/constantsRESTaSSUREDbdD/ConstantVideogame.java
@@ -3,4 +3,7 @@ package constantsRESTaSSUREDbdD;
 public interface ConstantVideogame {
 	public static final String URI_GETID = "/103";
 	public static final String BASE_URL = "http://localhost:8080/app/videogames";
+	
+	
+	
 }
